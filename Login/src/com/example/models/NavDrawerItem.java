03b014e9f5a99f0aa5d84b3db333ff0a44066b6e
PlYovchev <plt3ch@gmail.com>
2014-10-12 10:40:00 +0300

    Nav drawer added

---
+++ Login/src/com/example/models/NavDrawerItem.java
@@ -0,0 +1,30 @@
+package com.example.models;
+
+public class NavDrawerItem {
+	
+	private String title;
+    private int icon;
+     
+    public NavDrawerItem(){}
+ 
+    public NavDrawerItem(String title, int icon){
+        this.title = title;
+        this.icon = icon;
+    }
+    
+    public String getTitle(){
+        return this.title;
+    }
+    
+    public void setTitle(String title){
+        this.title = title;
+    }
+     
+    public int getIcon(){
+        return this.icon;
+    }
+          
+    public void setIcon(int icon){
+        this.icon = icon;
+    }
+}
